b9d79802b55e3b3f10b9e62dfbfddd01cd75572e
fire1774 <57627559+fire1774@users.noreply.github.com>
2023-04-04 16:24:00 -0500

    Update aba3.lua

--- aba3.lua
+++ aba3.lua
@@ -1,25 +1,19 @@
 repeat wait() until game.Loaded
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 setfpscap(30)
-task.wait(1)
-
 _G.MinTimer = 60
-
-
 local function serverHop()
 	if syn then
 		syn.queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/fire1774/abafarm/main/aba3.lua", true))()')
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fire1774/serverhop/main/serverhop.lua", true))()
 	end
 end
-
-
 local function timerCheck()
+	repeat wait() until game.Workspace:FindFirstChild("Message")
 	local timer = game.Workspace.Message.Text:split(" ")
 	local time = tonumber(timer[1])
 	return time
 end
-
 if timerCheck() <= _G.MinTimer then
     repeat task.wait() until timerCheck() > _G.MinTimer
     task.wait(1)
